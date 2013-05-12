4f59130de0ace9ff4c51c334348741d5767e59b4
Jim Geurts <jim@biacreations.com>
2013-05-12 11:06:01 -0500

    Update the description a bit

--- CacheStack/Properties/AssemblyInfo.cs
+++ CacheStack/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CacheStack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cache extensions for ServiceStack ICacheClient. Includes cache context and mvc donut caching.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CacheStack")]
