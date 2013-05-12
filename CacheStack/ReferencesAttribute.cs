0539daee807fb143866cb19e2e96fa77eed65f23
Jim Geurts <jim@biacreations.com>
2013-05-12 10:41:34 -0500

    Added a ReferencesAttribute so that Spruce or OrmLite are not required for Relation triggers

---
+++ CacheStack/ReferencesAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CacheStack
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Struct)]
+	public class ReferencesAttribute : Attribute
+	{
+		public Type Type { get; set; }
+
+		public ReferencesAttribute(Type type)
+		{
+			Type = type;
+		}
+	}
+}
